e9b9c36f56563b107c188e7c0db29207a5559e12
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-05-03 23:39:15 -0400

    I think I'm finished

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
+const User = require("../users/user-model.js")
+const bcrypt = require("bcryptjs")
+const secret = require("../config/secret.js")
+const jwt = require("jsonwebtoken")
 
 router.post('/register', (req, res) => {
   // implement registration
+  const user= req.body
+  user.password = bcrypt.hashSync(user.password, 10)
+  User.insertUser(user)
+  .then(newUser =>{
+    user.id=newUser[0]
+    delete user.password
+    const token = generateToken(user)
+    res.status(201).json(user, token)
+  })
+  .catch(err =>{
+    res.status(500).json({message: "There was a problem adding a new user", err})
+  })
+  
 });
 
 router.post('/login', (req, res) => {
   // implement login
-});
+  const {username, password} = req.body
 
+  User.findBy({ username })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+             const token = generateToken(user) 
+             res.status(200).json({ username, token })
+          } else {
+              res.status(401).json({ message: "Uh oh, something went wrong. Invalid credentials" })
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ message: "An error occured while logging in", err })
+      })
+});
+function generateToken(user) {
+  const payload = { 
+      userId: user.id,
+      username: user.username
+  }
+  const options = {
+      expiresIn: "2d"
+  }
+  const token = 
+      jwt.sign(payload, secret.jwtSecret, options)
+      return token
+  
+}
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secret')
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization
+  if (toke) {
+    jwt.verify(token, secrets.jwtSecret,(error, decodedToken) =>{
+      if (error){
+        console.log(error)
+        res.status(401).json({ you: 'shall not pass!' })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  } else {
+    res.status(500).json({message: 'You are not logged in, please try logging in', err})
+  }
 };
